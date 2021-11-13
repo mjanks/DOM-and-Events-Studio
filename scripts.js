b489da47ead6d3e8446d69f32f05f9e195cba907
mjanks <mjanks10@gmail.com>
2021-11-13 11:42:40 -0500

    added refs to buttons

--- scripts.js
+++ scripts.js
@@ -1,2 +1,11 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener('load', () => {
+  const takeOffButton = document.getElementById('takeoff');
+  const landButton = document.getElementById('takeoff');
+  const abortMissionButton = document.getElementById('missionAbort');
+  const upButton = document.getElementById('up');
+  const downButton = document.getElementById('down');
+  const leftButton = document.getElementById('left');
+  const rightButton = document.getElementById('right');
+});
